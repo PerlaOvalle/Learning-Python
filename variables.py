bae25d76b55c489dd5058bec14d00233f3b9976b
Perla Ovalle <perlaovalle@MacBook-Air-de-Edgar.local>
2021-03-24 21:43:55 -0600

    Ejercicio Variables

---
+++ variables.py
@@ -0,0 +1,9 @@
+#camelCase poner primer letra minuscula y segunda letra mayuscula
+primerCajonDelCloset = 'calcetines'
+
+segundoCajonDelCloset= 'calzones'
+
+#imprimir en python
+
+print(primerCajonDelCloset)
+print(segundoCajonDelCloset)
