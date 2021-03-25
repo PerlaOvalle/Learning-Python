f928c65408b84f16ae361debc05c1091ef782031
Perla Ovalle <perlaovalle@MacBook-Air-de-Edgar.local>
2021-03-24 22:19:14 -0600

    listas

---
+++ listas.py
@@ -0,0 +1,20 @@
+
+lista = [9 , 5,11, 8]
+
+print(sorted(lista, reverse = True))
+
+
+print(lista[0])
+
+lista.remove(11)
+print(lista)
+
+
+print(range(1, 10))
+
+lista =list(range(1,13, 3))
+print(lista)
+
+
+for valor in lista: 
+    print(valor)
